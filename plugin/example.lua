9f92df8ab4c01938bf73bf6ab197684c45a34f1f
Eric Pritchett <eric.a.pritchett.che@gmail.com>
2025-10-30 22:25:15 -0500

    start my neovim config journey

---
+++ plugin/example.lua
@@ -0,0 +1,5 @@
+-- This function will be available to execute
+-- via :lua ExampleFoobar()
+function ExampleFoobar()
+  print 'foobar'
+end
---
+++ plugin/test.lua
@@ -0,0 +1,4 @@
+-- This function code will be available in the global scope
+function TestFoobar()
+  print 'foobar'
+end
